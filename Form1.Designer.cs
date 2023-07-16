cfb80066c8b18a6cf4efaa7a471b9ca53802987e
Dede Adam Alamsyah <dedeadamalamsyah9@gmail.com>
2023-07-17 01:04:12 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿
+namespace pertemuan3_desktop
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.adx = new System.Windows.Forms.PictureBox();
+            this.musuh1 = new System.Windows.Forms.PictureBox();
+            this.musuh3 = new System.Windows.Forms.PictureBox();
+            this.musuh2 = new System.Windows.Forms.PictureBox();
+            this.over = new System.Windows.Forms.Label();
+            this.gold = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.silver = new System.Windows.Forms.PictureBox();
+            this.bronze = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.adx)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.musuh1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.musuh3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.musuh2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gold)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.silver)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bronze)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(226, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(17, 101);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Location = new System.Drawing.Point(226, 177);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(17, 101);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.White;
+            this.pictureBox3.Location = new System.Drawing.Point(226, 358);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(17, 101);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.White;
+            this.pictureBox4.Location = new System.Drawing.Point(226, 540);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(17, 101);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // adx
+            // 
+            this.adx.BackColor = System.Drawing.Color.Transparent;
+            this.adx.Image = ((System.Drawing.Image)(resources.GetObject("adx.Image")));
+            this.adx.Location = new System.Drawing.Point(212, 523);
+            this.adx.Name = "adx";
+            this.adx.Size = new System.Drawing.Size(50, 100);
+            this.adx.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.adx.TabIndex = 4;
+            this.adx.TabStop = false;
+            // 
+            // musuh1
+            // 
+            this.musuh1.BackColor = System.Drawing.Color.Transparent;
+            this.musuh1.Image = ((System.Drawing.Image)(resources.GetObject("musuh1.Image")));
+            this.musuh1.Location = new System.Drawing.Point(63, 242);
+            this.musuh1.Name = "musuh1";
+            this.musuh1.Size = new System.Drawing.Size(50, 100);
+            this.musuh1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.musuh1.TabIndex = 5;
+            this.musuh1.TabStop = false;
+            // 
+            // musuh3
+            // 
+            this.musuh3.BackColor = System.Drawing.Color.Transparent;
+            this.musuh3.Image = ((System.Drawing.Image)(resources.GetObject("musuh3.Image")));
+            this.musuh3.Location = new System.Drawing.Point(358, 374);
+            this.musuh3.Name = "musuh3";
+            this.musuh3.Size = new System.Drawing.Size(50, 100);
+            this.musuh3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.musuh3.TabIndex = 6;
+            this.musuh3.TabStop = false;
+            // 
+            // musuh2
+            // 
+            this.musuh2.BackColor = System.Drawing.Color.Transparent;
+            this.musuh2.Image = ((System.Drawing.Image)(resources.GetObject("musuh2.Image")));
+            this.musuh2.Location = new System.Drawing.Point(249, 121);
+            this.musuh2.Name = "musuh2";
+            this.musuh2.Size = new System.Drawing.Size(50, 100);
+            this.musuh2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.musuh2.TabIndex = 7;
+            this.musuh2.TabStop = false;
+            // 
+            // over
+            // 
+            this.over.BackColor = System.Drawing.Color.Transparent;
+            this.over.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.over.ForeColor = System.Drawing.Color.Red;
+            this.over.Location = new System.Drawing.Point(134, 177);
+            this.over.Name = "over";
+            this.over.Size = new System.Drawing.Size(207, 44);
+            this.over.TabIndex = 8;
+            this.over.Text = "Game Over";
+            // 
+            // gold
+            // 
+            this.gold.BackColor = System.Drawing.Color.Transparent;
+            this.gold.Image = ((System.Drawing.Image)(resources.GetObject("gold.Image")));
+            this.gold.Location = new System.Drawing.Point(249, 29);
+            this.gold.Name = "gold";
+            this.gold.Size = new System.Drawing.Size(70, 45);
+            this.gold.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.gold.TabIndex = 9;
+            this.gold.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(3, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 45);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Score: 0";
+            // 
+            // silver
+            // 
+            this.silver.BackColor = System.Drawing.Color.Transparent;
+            this.silver.Image = ((System.Drawing.Image)(resources.GetObject("silver.Image")));
+            this.silver.Location = new System.Drawing.Point(65, 129);
+            this.silver.Name = "silver";
+            this.silver.Size = new System.Drawing.Size(70, 45);
+            this.silver.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.silver.TabIndex = 14;
+            this.silver.TabStop = false;
+            // 
+            // bronze
+            // 
+            this.bronze.BackColor = System.Drawing.Color.Transparent;
+            this.bronze.Image = ((System.Drawing.Image)(resources.GetObject("bronze.Image")));
+            this.bronze.Location = new System.Drawing.Point(347, 210);
+            this.bronze.Name = "bronze";
+            this.bronze.Size = new System.Drawing.Size(70, 45);
+            this.bronze.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bronze.TabIndex = 15;
+            this.bronze.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(178, 274);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 34);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Restart";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(178, 343);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 34);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(478, 644);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.bronze);
+            this.Controls.Add(this.silver);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gold);
+            this.Controls.Add(this.over);
+            this.Controls.Add(this.musuh2);
+            this.Controls.Add(this.musuh3);
+            this.Controls.Add(this.musuh1);
+            this.Controls.Add(this.adx);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.Color.Transparent;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.adx)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.musuh1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.musuh3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.musuh2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gold)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.silver)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bronze)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox adx;
+        private System.Windows.Forms.PictureBox musuh1;
+        private System.Windows.Forms.PictureBox musuh3;
+        private System.Windows.Forms.PictureBox musuh2;
+        private System.Windows.Forms.Label over;
+        private System.Windows.Forms.PictureBox gold;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox silver;
+        private System.Windows.Forms.PictureBox bronze;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows.Forms;
+
+namespace pertemuan3_desktop
+{
+    public partial class Form1 : System.Windows.Forms.Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            over.Visible = false;
+            button1.Visible = false;
+            button2.Visible = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            line(kecepatan);
+            musuh(kecepatan);
+            coins(kecepatan);
+            gameover();
+            scores();
+            restart();
+        }
+
+        Random r = new Random();
+        int x;
+        void musuh(int speed)
+        {
+            if (musuh1.Top >= 700)
+            {
+                x = r.Next(0, 400);
+                musuh1.Location = new Point(x, 0);
+            }
+            else
+            {
+                musuh1.Top += speed;
+            }
+
+            if (musuh2.Top >= 700)
+            {
+                x = r.Next(0, 400);
+                musuh2.Location = new Point(x, 0);
+            }
+            else
+            {
+                musuh2.Top += speed;
+            }
+
+            if (musuh3.Top >= 700)
+            {
+                x = r.Next(0, 400);
+                musuh3.Location = new Point(x, 0);
+            }
+            else
+            {
+                musuh3.Top += speed;
+            }
+        }
+
+        void coins(int speed)
+        {
+            if (gold.Top >= 700)
+            {
+                x = r.Next(0, 400);
+                gold.Location = new Point(x, 0);
+            }
+            else
+            {
+                gold.Top += speed;
+            }
+
+            if (silver.Top >= 700)
+            {
+                x = r.Next(0, 400);
+                silver.Location = new Point(x, 0);
+            }
+            else
+            {
+                silver.Top += speed;
+            }
+
+            if (bronze.Top >= 700)
+            {
+                x = r.Next(0, 400);
+                bronze.Location = new Point(x, 0);
+            }
+            else
+            {
+                bronze.Top += speed;
+            }
+        }
+
+        void gameover()
+        {
+            if (adx.Bounds.IntersectsWith(musuh1.Bounds))
+            {
+                timer1.Enabled = false;
+                over.Visible = true;
+            }
+            if (adx.Bounds.IntersectsWith(musuh2.Bounds))
+            {
+                timer1.Enabled = false;
+                over.Visible = true;
+            }
+            if (adx.Bounds.IntersectsWith(musuh3.Bounds))
+            {
+                timer1.Enabled = false;
+                over.Visible = true;
+            }
+        }
+
+        int score = 0;
+        void scores()
+        {
+            if (adx.Bounds.IntersectsWith(gold.Bounds))
+            {
+                score += 35;
+
+                x = r.Next(0, 400);
+                gold.Location = new Point(x, 0);
+            }
+            if (adx.Bounds.IntersectsWith(silver.Bounds))
+            {
+                score += 20;
+
+                x = r.Next(0, 400);
+                silver.Location = new Point(x, 0);
+            }
+            if (adx.Bounds.IntersectsWith(bronze.Bounds))
+            {
+                score += 10;
+
+                x = r.Next(0, 400);
+                bronze.Location = new Point(x, 0);
+            }
+            label1.Text = "Score: " + score.ToString();
+        }
+
+
+
+        void line(int speed)
+        {
+            if (pictureBox1.Top >= 700)
+            {
+                pictureBox1.Top = 0;
+            }
+            else
+            {
+                pictureBox1.Top += speed;
+            }
+
+            if (pictureBox2.Top >= 700)
+            {
+                pictureBox2.Top = 0;
+            }
+            else
+            {
+                pictureBox2.Top += speed;
+            }
+
+            if (pictureBox3.Top >= 700)
+            {
+                pictureBox3.Top = 0;
+            }
+            else
+            {
+                pictureBox3.Top += speed;
+            }
+
+            if (pictureBox4.Top >= 700)
+            {
+                pictureBox4.Top = 0;
+            }
+            else
+            {
+                pictureBox4.Top += speed;
+            }
+        }
+
+        int kecepatan = 5;
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                if (adx.Left > 0)
+                    adx.Left -= kecepatan;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                if (adx.Right < 480)
+                    adx.Left += kecepatan;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                if (kecepatan < 20)
+                {
+                    kecepatan++;
+                }
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                if (kecepatan > 0)
+                {
+                    kecepatan--;
+                }
+            }
+
+        }
+
+        void restart()
+        {
+            if(over.Visible)
+            {
+                button1.Visible = true;
+                button2.Visible = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
